cd48949ab9c9a49851d8315c1c709de05a58be0e
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2025-05-19 17:02:10 +0300

    teht 1.3-1.4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -35,9 +35,18 @@ const App = () => {
   const course = 'Half Stack application development'
 
   const parts = [
-    { name: 'Fundamentals of React', exercises: 10},
-    { name: 'Using props to pass data', exercises: 7},
-    { name: 'State of a component', exercises: 14}
+    { 
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    { 
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
 
   return (
