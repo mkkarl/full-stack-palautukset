eb111a36f66fc97ca8fb2eeb6a7bfaa63035af39
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2025-06-16 22:04:38 +0300

    teht 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -16,6 +16,23 @@ const Display = (props) => {
   )
 }
 
+const Statistics = props => {
+  const sum = props.goodClicks + props.neutralClicks + props.badClicks
+  const average = (props.goodClicks - props.badClicks) / sum
+  const positive = props.goodClicks / sum * 100
+
+  return (
+    <p>
+      good {props.goodClicks}<br />
+      neutral {props.neutralClicks}<br />
+      bad {props.badClicks}<br/>
+      all {sum}<br/>
+      average {average}<br />
+      positive {positive} %
+    </p>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -43,6 +60,7 @@ const App = () => {
       <Display text='good' clicks={good} />
       <Display text='neutral' clicks={neutral} />
       <Display text='bad' clicks={bad} />
+      <Statistics goodClicks={good} neutralClicks={neutral} badClicks={bad}/>
     </div>
   )
 }
