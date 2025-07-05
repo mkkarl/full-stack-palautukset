3de0c5dcf08efddd74ea02a403eaa4a9deb92a4b
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2025-07-05 20:28:34 +0300

    teht 3.11 nettiin

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -13,6 +13,7 @@ morgan.token('postreq', (req, res) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postreq'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
