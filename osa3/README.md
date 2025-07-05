1525c73c9687ab37b6410f50d0eaf2e75f7cb527
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2025-07-05 16:45:37 +0300

    teht 3.10 portti envistä

--- osa3/README.md
+++ osa3/README.md
@@ -14,7 +14,7 @@ Osasta 3 on tehty seuraavat tehtävät:
 - [x] 3.6
 - [x] 3.7
 - [x] 3.8*
-- [ ] 3.9
+- [x] 3.9
 - [ ] 3.10
 - [ ] 3.11
 - [ ] 3.12
--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -104,7 +104,7 @@ app.get('/info', (request, response) => {
     response.send(`<div>${info}${time}</div>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
