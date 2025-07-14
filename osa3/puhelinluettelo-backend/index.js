44a0860403cadacdb41ee2e8d85f12d22a4e089a
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2025-07-14 14:08:15 +0300

    teht 3.16

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -11,9 +11,20 @@ morgan.token('postreq', (req, res) => {
     return ''
 })
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        response.status(400).send({ error: "malformatted id" })
+    }
+
+    next(error)
+}
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postreq'))
 app.use(express.static('dist'))
+app.use(errorHandler)
 
 let persons = [
     {
@@ -79,10 +90,16 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch (error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -90,7 +107,7 @@ app.delete('/api/persons/:id', (request, response) => {
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => next(error))    
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
